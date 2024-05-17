41f3314cd2afeeabfc0ab2d28b80f55537c40e18
Daniil Dzonson <daniildzonson04@gmail.com>
2024-05-17 12:24:16 +0300

    -

--- odoo_service.py
+++ odoo_service.py
@@ -1,5 +1,6 @@
 def get_service(
-    admin_password="Parole.Ogre777"
+    admin_password="Parole.Ogre777",
+    venv_path=""
     ):
     s = f"""
 [Unit]
@@ -12,7 +13,7 @@ SyslogIdentifier=odoo
 PermissionsStartOnly=true
 User=odoo16
 Group=odoo16
-ExecStart=/odoo/16/python3 /odoo/16/odoo-bin -c /etc/odoo16.conf
+ExecStart={venv_path}bin/python3 /odoo/16/odoo-bin -c /etc/odoo16.conf
 StandardOutput=journal+console
 [Install]
 WantedBy=multi-user.target
--- prepare_script.py
+++ prepare_script.py
@@ -13,38 +13,6 @@ venv_name = "odoo16-venv"
 conf_name = "odoo16.conf"
 service_name = "odoo16.service"
 
-psql_commands = f"""
-alter user {postgres_user} with encrypted password '{postgres_user_pass}';
-\\q
-"""
-
-""" process = subprocess.Popen("sudo apt update && sudo apt upgrade", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-process.stdin.write(f'sudo useradd -m -d {linux_user_home} -U -r -s /usr/sbin/nologin {linux_user}')
-process.stdin.write(f'sudo apt install build-essential wget git python3-pip python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libsasl2-dev python3-setuptools libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libopenjp2-7-dev')
-process.stdin.write(f'sudo apt-get install postgresql')
-process.stdin.write(f'sudo su - postgres -c "createuser -s {postgres_user}"')
-process.stdin.write("sudo -u postgres psql")
-process.stdin.write(psql_commands)
-process.stdin.write(f"sudo apt install wkhtmltopdf")
-process.stdin.write(f"sudo su - {linux_user} -s /bin/bash")
-process.stdin.write(f"git clone https://www.github.com/odoo/odoo --depth 1 --branch {odoo_version} {odoo_path}")
-process.stdin.write(f"cd {odoo_path}")
-process.stdin.write(f"python3 -m venv {venv_name}")
-process.stdin.write(f"source {venv_name}/bin/activate")
-process.stdin.write(f"pip3 install wheel")
-process.stdin.write(f"mkdir custom-addons")
-process.stdin.write(f"exit")
-process = subprocess.Popen(f"sudo su - {linux_user} -s /bin/bash", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-process.stdin.write(f"git clone https://www.github.com/odoo/odoo --depth 1 --branch {odoo_version} {odoo_path}")
-process.stdin.write(f"cd {odoo_path}")
-process.stdin.write(f"python3 -m venv {venv_name}")
-process.stdin.write(f"source {venv_name}/bin/activate")
-process.stdin.write(f"pip3 install wheel")
-process.stdin.write(f"mkdir custom-addons")
-process.stdin.write(f"exit") """
-
-
-
 sh_script = f"""
 #!/bin/bash
 
@@ -58,6 +26,7 @@ sudo apt install wkhtmltopdf
 sudo su - odoo16 -s /bin/bash -c "git clone https://www.github.com/odoo/odoo --depth 1 --branch {odoo_version} {odoo_path}"
 sudo su - odoo16 -s /bin/bash -c "python3 -m venv {linux_user_home}/{odoo_path}/{venv_name}"
 sudo su - odoo16 -s /bin/bash -c "source {linux_user_home}/{odoo_path}/{venv_name}/bin/activate && pip3 install wheel && pip3 install -r {linux_user_home}/{odoo_path}/requirements.txt"
+sudo su - odoo16 -s /bin/bash -c "mkdir custom-addons"
 
 sudo systemctl daemon-reload
 sudo systemctl start {service_name}
@@ -71,4 +40,4 @@ with open(f"/etc/{conf_name}","w") as f:
     f.write(get_conf())
 
 with open(f"/etc/systemd/system/{service_name}","w") as f:
-    f.write(get_service())
+    f.write(get_service(venv_path=f"{linux_user_home}/{odoo_path}/{venv_name}/"))
