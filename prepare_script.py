9fae64c7e6acf85fb76094362ed7a9c4e6527bb0
Daniil Dzonson <daniildzonson04@gmail.com>
2024-05-17 12:06:30 +0300

    -

--- prepare_script.py
+++ prepare_script.py
@@ -48,6 +48,7 @@ process.stdin.write(f"exit") """
 sh_script = f"""
 #!/bin/bash
 
+sudo useradd -m -d {linux_user_home} -U -r -s /usr/sbin/nologin {linux_user}
 sudo apt install build-essential wget git python3-pip python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libsasl2-dev python3-setuptools libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libopenjp2-7-dev
 sudo apt-get install postgresql
 sudo su - postgres -c "createuser -s {postgres_user}"
@@ -56,9 +57,7 @@ sudo apt install wkhtmltopdf
 
 sudo su - odoo16 -s /bin/bash -c "git clone https://www.github.com/odoo/odoo --depth 1 --branch {odoo_version} {odoo_path}"
 sudo su - odoo16 -s /bin/bash -c "python3 -m venv {linux_user_home}/{odoo_path}/{venv_name}"
-source {linux_user_home}/{odoo_path}/{venv_name}/bin/activate
-pip3 install wheel
-pip3 install -r {linux_user_home}/{odoo_path}/requirements.txt
+sudo su - odoo16 -s /bin/bash -c "source {linux_user_home}/{odoo_path}/{venv_name}/bin/activate && pip3 install wheel && pip3 install -r {linux_user_home}/{odoo_path}/requirements.txt"
 
 sudo systemctl daemon-reload
 sudo systemctl start {service_name}
