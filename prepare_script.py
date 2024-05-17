bbf1a499c4de66ee518c63465e3ed6c850ead6c9
Daniil Dzonson <daniildzonson04@gmail.com>
2024-05-17 12:31:50 +0300

    -

--- prepare_script.py
+++ prepare_script.py
@@ -17,11 +17,11 @@ sh_script = f"""
 #!/bin/bash
 
 sudo useradd -m -d {linux_user_home} -U -r -s /usr/sbin/nologin {linux_user}
-sudo apt install build-essential wget git python3-pip python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libsasl2-dev python3-setuptools libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libopenjp2-7-dev
-sudo apt-get install postgresql
+sudo apt install -y build-essential wget git python3-pip python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libsasl2-dev python3-setuptools libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libopenjp2-7-dev
+sudo apt-get install postgresql -y
 sudo su - postgres -c "createuser -s {postgres_user}"
 sudo su - postgres -c "alter user {postgres_user} with encrypted password '{postgres_user_pass}'"
-sudo apt install wkhtmltopdf
+sudo apt install wkhtmltopdf -y
 
 sudo su - odoo16 -s /bin/bash -c "git clone https://www.github.com/odoo/odoo --depth 1 --branch {odoo_version} {odoo_path}"
 sudo su - odoo16 -s /bin/bash -c "python3 -m venv {linux_user_home}/{odoo_path}/{venv_name}"
