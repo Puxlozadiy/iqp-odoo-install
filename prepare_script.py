5a96d7e21971745e1c206993507027ca04e3858c
Daniil Dzonson <daniildzonson04@gmail.com>
2024-05-20 15:30:09 +0300

    -

--- prepare_script.py
+++ prepare_script.py
@@ -22,11 +22,10 @@ sh_script = f"""
 #!/bin/bash
 
 sudo useradd -m -d {linux_user_home} -U -r -s /usr/sbin/nologin {linux_user}
-sudo apt install -y build-essential wget git python3-pip python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libsasl2-dev python3-setuptools libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libopenjp2-7-dev
-sudo apt-get install postgresql -y
+sudo apt install -y wkhtmltopdf build-essential wget git python3-pip python3-dev python3-venv python3-wheel libfreetype6-dev libxml2-dev libzip-dev libsasl2-dev python3-setuptools libjpeg-dev zlib1g-dev libpq-dev libxslt1-dev libldap2-dev libtiff5-dev libopenjp2-7-dev
+sudo apt-get install -y postgresql unzip
 sudo su - postgres -c "createuser -s {postgres_user}"
 sudo -u postgres psql -c "alter user {postgres_user} with encrypted password '{postgres_user_pass}'"
-sudo apt install wkhtmltopdf -y
 
 sudo su - {linux_user} -s /bin/bash -c "git clone https://www.github.com/odoo/odoo --depth 1 --branch {odoo_version} {odoo_path}"
 sudo su - {linux_user} -s /bin/bash -c "python3 -m venv {linux_user_home}/{odoo_path}/{venv_name}"
@@ -35,10 +34,17 @@ sudo su - {linux_user} -s /bin/bash -c "mkdir {odoo_path}/custom-addons"
 
 sudo systemctl daemon-reload
 sudo systemctl start {service_name}
+
+unzip default-addons.zip
 """
 
 with open(f"./install.sh","w") as f:
     f.write(sh_script)
+    
+with open(f"./copy-addons.sh","w") as f:
+    f.write(f"""sudo cp -r default-addons/. {linux_user_home}/{odoo_path}/custom-addons
+sudo chown {linux_user} {linux_user_home}/{odoo_path}/custom-addons
+""")
 
 
 with open(f"/etc/{conf_name}","w") as f:
